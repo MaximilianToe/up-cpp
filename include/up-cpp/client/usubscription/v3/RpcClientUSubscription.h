bf8758b795227116b9fa506ad56efbd32c9d2a33
Max <mtoenies@outlook.de>
2025-05-10 11:26:50 +0200

    changed access modifier for builder to public

--- include/up-cpp/client/usubscription/v3/RpcClientUSubscription.h
+++ include/up-cpp/client/usubscription/v3/RpcClientUSubscription.h
@@ -218,6 +218,7 @@ private:
 	//     std::shared_ptr<uprotocol::transport::UTransport>&&, const v1::UUri&&,
 	//     RpcClientUSubscriptionOptions&&);
 
+public:
 	/// @brief Build SubscriptionRequest for subscription request
 	SubscriptionRequest buildSubscriptionRequest();
 	//
