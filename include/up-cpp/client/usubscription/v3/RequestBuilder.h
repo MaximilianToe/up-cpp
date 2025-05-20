f9fa701d683a24aa943c6df996fd4f45c4e0b2ea
Lennart Becker <lennart.becker@d-fine.com>
2025-05-20 18:39:03 +0200

    Added comments in RequestBuilder.h

--- include/up-cpp/client/usubscription/v3/RequestBuilder.h
+++ include/up-cpp/client/usubscription/v3/RequestBuilder.h
@@ -29,17 +29,43 @@ struct USubscriptionOptions {
 	std::optional<google::protobuf::Any> subscription_details;
 };
 
-	struct RequestBuilder {
-		 explicit RequestBuilder(
-	        USubscriptionOptions  options = {})
-			:options_(std::move(options)){};
+/**
+ * @brief Builds different requests using specified options.
+ *
+ * This struct facilitates the construction of requests based on `USubscriptionOptions`,
+ * providing methods to build different requests.
+ */
+struct RequestBuilder {
+    /**
+     * @brief Constructs a RequestBuilder with the given subscription options.
+     * 
+     * @param options Subscription options to configure the requests. Defaults to empty options.
+     */
+    explicit RequestBuilder(USubscriptionOptions options = {}) 
+        : options_(std::move(options)) {}
 
-		SubscriptionRequest buildSubscriptionRequest(const v1::UUri& topic) const;
+    /**
+     * @brief Builds a subscription request for a given topic.
+     * 
+     * @param topic The `v1::UUri` representing the topic for the subscription.
+     * 
+     * @return A `SubscriptionRequest` configured for the specified topic.
+     */
+    SubscriptionRequest buildSubscriptionRequest(const v1::UUri& topic) const;
 
-		UnsubscribeRequest buildUnsubscribeRequest(const v1::UUri& topic);
-	private:
-		USubscriptionOptions options_;
-	};
+    /**
+     * @brief Builds an unsubscription request for a given topic.
+     * 
+     * @param topic The `v1::UUri` representing the topic to unsubscribe from.
+     * 
+     * @return An `UnsubscribeRequest` configured for the specified topic.
+     */
+    UnsubscribeRequest buildUnsubscribeRequest(const v1::UUri& topic) const;
+
+private:
+    USubscriptionOptions options_; ///< Options used to configure the requests.
+};
+
 
 } // namespace uprotocol::core::usubscription::v3
 #endif //REQUESTBUILDER_H
