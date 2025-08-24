17ea8bbbc45b8649dec119a644fd1a200725257c
Max <mtoenies@outlook.de>
2025-08-24 18:19:55 +0200

    locally copies builder_ in invokeMethod for thread safety

--- src/communication/RpcClient.cpp
+++ src/communication/RpcClient.cpp
@@ -208,13 +208,18 @@ RpcClient::InvokeHandle RpcClient::invokeMethod(v1::UMessage&& request,
 RpcClient::InvokeHandle RpcClient::invokeMethod(
     const v1::UUri& method, datamodel::builder::Payload&& payload,
     Callback&& callback) {
-	return invokeMethod(builder_.withMethod(method).build(std::move(payload)),
-	                    std::move(callback));
+	// makes a copy of the builder with default config to avoid race conditions
+	auto local_builder = builder_;
+	return invokeMethod(
+	    local_builder.withMethod(method).build(std::move(payload)),
+	    std::move(callback));
 }
 
 RpcClient::InvokeHandle RpcClient::invokeMethod(const v1::UUri& method,
                                                 Callback&& callback) {
-	return invokeMethod(builder_.withMethod(method).build(),
+	// makes a copy of the builder with default config to avoid race conditions
+	auto local_builder = builder_;
+	return invokeMethod(local_builder.withMethod(method).build(),
 	                    std::move(callback));
 }
 
